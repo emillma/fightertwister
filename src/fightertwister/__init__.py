87ffd78f80c1cc897eef80ccd713ce0c461fb163
Emil Laptop <emil.martens@gmail.com>
2021-06-13 14:52:53 +0200

    small fix

--- src/fightertwister/__init__.py
+++ src/fightertwister/__init__.py
@@ -1,6 +1,6 @@
 from .fightertwister import FighterTwister
 from .encoder import Encoder
-from .utils import ft_colors, heat_color
+from .utils import ft_colors, heat_color, to_range
 from .ftbro import FtBro
 from .button import Button
 from .ftcollections import EncoderCollection, ButtoCollection
